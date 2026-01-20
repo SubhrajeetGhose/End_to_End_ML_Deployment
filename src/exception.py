8d2a8772f7c32dda452bffb15df2bebe158f677f
Subhrajit Ghose <subhrajeetghose5@gmail.com>
2026-01-20 23:41:06 +0530

    loggin

--- src/exception.py
+++ src/exception.py
@@ -16,9 +16,3 @@ def CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__ == "__main__":
-    try:
-        a = 1 / 0
-    except Exception as e:
-        logging.info("Divided by zero error occurred")
-        raise CustomException(e)
